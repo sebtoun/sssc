25bff1effad79e9f355b1db3330f6e9fb0354efa
Sebastien Andary <sebastien.andary@gmail.com>
2017-11-09 21:03:07 +0100

    adds leader board

---
+++ assets/javascripts/leaderboard.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// <li><span class="score">1000</span> <span class="playername">AAA</span></li>
+
+var loadLeaderBoard = function() {
+    var url = "https://wt-a89f61fd25d1773a3cff1712ca7d1c33-0.run.webtask.io/sssc-leaderboard";
+
+    function createScoreListEntry(scoreEntry) {
+        var li = document.createElement('li');
+
+        var score = document.createElement('span');
+        score.className = 'score';
+        score.textContent = scoreEntry.score;
+
+        var name = document.createElement('span');
+        name.className = 'playername';
+        name.textContent = scoreEntry.playername;
+        li.appendChild(score);
+        li.appendChild(document.createTextNode(" "));
+        li.appendChild(name);
+
+        return li;
+    }
+
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function () {
+        if (r.readyState === XMLHttpRequest.DONE) {
+            if (r.status !== 200) return;
+
+            var highscores = JSON.parse(r.responseText).highscores;
+            if (highscores){
+                var list = document.getElementById('leaderboard').querySelector('ul.leaderboard');
+                var fragment = document.createDocumentFragment();
+                highscores.forEach(function (entry) {
+                    var item = createScoreListEntry(entry);
+                    fragment.appendChild(item);
+                });
+                list.appendChild(fragment);
+            }
+        }
+    };
+    r.open("GET", url, true);
+    r.send(null);
+};
--- index.md
+++ index.md
@@ -6,15 +6,4 @@ TODO
 
 ---------------------
 
-<div class="webgl-content">
-    <div id="gameContainer" style="width: 960px; height: 600px"></div>
-    <div class="footer">
-        <div class="fullscreen" onclick="gameInstance.SetFullscreen(1)"></div>
-    </div>
-</div>
-
----------------------
-
-Après avoir joué, répondez à un petit [questionnaire](https://docs.google.com/forms/d/e/1FAIpQLSdrg0yLu6JQF4gIbiDN4vODoS3YsHGDhIoeIv_bIgFsZEhIIQ/viewform?usp=sf_link)
- 
-Merci !
+Après avoir joué, répondez à un petit [questionnaire](https://docs.google.com/forms/d/e/1FAIpQLSdrg0yLu6JQF4gIbiDN4vODoS3YsHGDhIoeIv_bIgFsZEhIIQ/viewform?usp=sf_link), merci !
